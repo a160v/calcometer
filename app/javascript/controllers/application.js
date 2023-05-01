692212bdc537494eaef1622b0e08ff7a1a03ebe8
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-01 22:50:39 +0200

    added tailwindcss-stimulus-components

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,29 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+// Import and register all TailwindCSS Components
+import {
+  Alert,
+  Autosave,
+  Dropdown,
+  Modal,
+  Tabs,
+  Popover,
+  Toggle,
+  Slideover,
+} from "tailwindcss-stimulus-components";
+application.register("alert", Alert);
+application.register("autosave", Autosave);
+application.register("dropdown", Dropdown);
+application.register("modal", Modal);
+application.register("tabs", Tabs);
+application.register("popover", Popover);
+application.register("toggle", Toggle);
+application.register("slideover", Slideover);
+
+export { application };
