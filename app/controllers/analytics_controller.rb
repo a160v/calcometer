e32124750fdd65c68d48ad9e795c8cc4c4855c9f
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-09 21:11:30 +0200

    code refactored for better safety

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -9,18 +9,20 @@ class AnalyticsController < ApplicationController
 
     @treatments = Treatment.joins(:patient)
                            .where(user_id: current_user.id)
-                           .where("start_time >= ? AND start_time <= ?", @start_date.beginning_of_day, @end_date.end_of_day)
+                           .where("start_time >= ? AND start_time <= ?",
+                            @start_date.beginning_of_day, @end_date.end_of_day)
     @total_distance = 0
     @total_time = 0
 
     # Calculate total distance and time starting from the first treatment
     # if there are more than two treatments
-    if @treatments.length >= 2
-      @treatments.each_cons(2) do |treatment1, treatment2|
-        @total_distance += calculate_distance(treatment1.patient.address, treatment2.patient.address)
-      end
-      @total_time = calculate_driving_time(@total_distance)
+    return unless @treatments.length >= 2
+
+    @treatments.each_cons(2) do |treatment1, treatment2|
+      @total_distance += calculate_distance(treatment1.patient.address, treatment2.patient.address)
     end
+
+    @total_time = calculate_driving_time(@total_distance)
   end
 
   private
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: [:show, :edit, :update, :destroy]
+  before_action :set_client, only: %i[show edit update destroy]
 
   def index
     @clients = Client.all
