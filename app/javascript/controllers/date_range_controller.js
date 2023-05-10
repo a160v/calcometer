a8385a5519db509b25d5fdf7493f2688ebbf38a5
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-10 03:09:31 +0200

    fix typos

--- app/javascript/controllers/date_range_controller.js
+++ app/javascript/controllers/date_range_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="date-range"
 export default class extends Controller {
+  connect() {
+    console.log("The 'date range' controller is connected");
+  }
   static targets = ["startDate", "endDate"];
 
   connect() {
