932f97ae32e4be244906eab001e20f8259287785
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-10 16:14:22 +0200

    refactor TreatmentsHelper including cachce and error handling if coordinates aren't available

--- app/helpers/treatments_helper.rb
+++ app/helpers/treatments_helper.rb
@@ -1,33 +1,38 @@
 module TreatmentsHelper
+  AVERAGE_SPEED = 50.0 # Average driving speed in Switzerland (in km/h)
+
   # Fetch the total distance driven by car between each treatment;
   # reiterate with all treatments for a given day
-  def calculate_distance(address1, address2)
 
+  def calculate_distance(address1, address2)
     # Geocoding the addresses to get coordinates
-    coordinates1 = Geocoder.coordinates(address1)
-    coordinates2 = Geocoder.coordinates(address2)
+    coordinates1 = fetch_coordinates(address1)
+    coordinates2 = fetch_coordinates(address2)
 
     # Check if coordinates are available
-    if coordinates1.nil? || coordinates2.nil?
-      raise "Coordinates not found for one or both addresses: #{address1}, #{address2}"
-    end
+    return 0.0 unless coordinates1 && coordinates2
 
     # Calculate distance using Geocoder::Calculations.distance_between
-    distance = Geocoder::Calculations.distance_between(coordinates1, coordinates2)
-
-    return distance
+    Geocoder::Calculations.distance_between(coordinates1, coordinates2)
   end
 
   def calculate_driving_time(distance)
-    # Average driving speed in Switzerland (in km/h)
-    average_speed = 50.0
-
     # Calculate driving time in hours
-    time_in_hours = distance.round(2) / average_speed
-
+    time_in_hours = distance.round(2) / AVERAGE_SPEED
     # Convert driving time to minutes
-    time_in_minutes = (time_in_hours * 60).round
+    (time_in_hours * 60).round
+  end
 
-    return time_in_minutes
+  private
+
+  def fetch_coordinates(address)
+    Rails.cache.fetch(address, expires_in: 1.week) do
+      Geocoder.coordinates(address) || log_unfound_coordinates(address)
+    end
+  end
+
+  def log_unfound_coordinates(address)
+    Rails.logger.warn "Coordinates not found for address: #{address}"
+    nil
   end
 end
