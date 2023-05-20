d6a137f83b773a9707c05174aa63af1a3407a82d
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-20 17:57:15 +0200

    update analytics controller after trip model logic

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,43 +1,35 @@
 class AnalyticsController < ApplicationController
-  include AppointmentsHelper
   before_action :set_dates, only: [:index]
 
-  # Display only today's appointments, sorted chronologically
   def index
     set_dates_to_today
-    set_time_and_distance_to_zero
-
-    # Calculate total distance and time starting from the first appointment
-    # if there are more than two appointments
-    return unless @appointments.length >= 2
-
-    @appointments.each_cons(2) do |appointment1, appointment2|
-      @total_distance += calculate_driving_distance(appointment1.patient.address, appointment2.patient.address)
-    end
-
-    @total_time = calculate_driving_time(@total_distance)
+    @total_distance = calculate_total_distance
+    @total_time = calculate_total_time
   end
 
   private
 
-  # Set the start and end dates to today's date
   def set_dates
     @start_date = params[:start_date] ? Date.parse(params[:start_date]) : Time.current.beginning_of_day
     @end_date = params[:end_date] ? Date.parse(params[:end_date]) : Time.current.end_of_day
   end
 
+  # Set the start and end dates to today's date
   def set_dates_to_today
     @start_date = params[:start_date]&.to_date || Time.current.to_date
     @end_date = params[:end_date]&.to_date || Time.current.to_date
 
     @appointments = Appointment.joins(:patient)
-                           .where(user_id: current_user.id)
-                           .where("start_time >= ? AND start_time <= ?",
-                            @start_date.beginning_of_day, @end_date.end_of_day)
+                               .where(user_id: current_user.id)
+                               .where("start_time >= ? AND start_time <= ?", @start_date.beginning_of_day, @end_date.end_of_day)
   end
 
-  def set_time_and_distance_to_zero
-    @total_distance = 0
-    @total_time = 0
+  # Calculate the total distance and total time using Trip model
+  def calculate_total_distance
+    Trip.where(start_appointment: @appointments).sum(:driving_distance)
+  end
+
+  def calculate_total_time
+    Trip.where(start_appointment: @appointments).sum(:driving_time)
   end
 end
