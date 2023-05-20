d819d81eb00b2efa2f31bdaff25df8269541448e
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-20 16:17:17 +0200

    update appointment validation with trip

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,15 +2,19 @@ class Appointment < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :patient, optional: true
   has_one :address, through: :patient
+  has_one :start_trip, class_name: 'Trip', foreign_key: 'start_appointment_id', dependent: :destroy
+  has_one :end_trip, class_name: 'Trip', foreign_key: 'end_appointment_id', dependent: :destroy
 
   validates :start_time, :end_time, presence: true
   validate :validate_time, :no_overlapping_appointments
+
+  after_destroy :recalculate_trip
 
   def validate_time
     errors.add(:end_time, "must be after start time") if
       end_time < start_time
 
-    errors.add(:end_time, "cannot be equal to start time") if
+    errors.add(:start_time, "cannot be equal to start time") if
       end_time == start_time
 
     errors.add(:start_time, "| You have already created another appointment for this time today.") if
@@ -27,5 +31,9 @@ class Appointment < ApplicationRecord
 
     errors.add(:start_time, "The appointment period overlaps with another existing appointment.") if
       overlapping_appointments.exists?
+  end
+
+  def recalculate_trip
+    RecalculateTripJob.perform_later(self.id)
   end
 end
