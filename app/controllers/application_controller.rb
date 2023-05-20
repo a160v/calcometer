692274c5b6645dbba5c23697b983417f0e8a7b54
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-20 13:25:45 +0200

    fix scss issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :last_name, :email, :password, :address)}
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :last_name, :email, :password, :address)}
 
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :last_name, :email, :address, :password, :current_password)}
-    end
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :last_name, :email, :address, :password, :current_password)}
+  end
 end
