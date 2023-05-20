06746fa80e9f44d1ac37d778f6a527966240d669
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-20 17:58:09 +0200

    remove scope from email validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,5 +2,5 @@ class Client < ApplicationRecord
   has_many :patients, dependent: :nullify
 
   validates :name, :email, presence: true
-  validates :email, uniqueness: { scope: :email, message: "already used by a client, please input a different one." }
+  validates :email, uniqueness: { message: "already used by a client, please input a different one." }
 end
