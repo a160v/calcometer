246e7edee1f8f3acbe9d3eb7a541b0dc27648bcf
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-23 18:00:54 +0200

    add email validation for clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,13 @@
 class Client < ApplicationRecord
   has_many :patients, dependent: :nullify
 
-  validates :name, :email, presence: true
-  # validates :email, uniqueness: { message: "already used by a client, please input a different one." }
+  validates :name, presence: true
+  validates :email,
+            presence: true,
+            format: {
+              with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+              message: 'Please provide a valid e-mail address'
+            },
+            on: :create,
+            uniqueness: { message: "already used by an existing client, please provide a different one." }
 end
