1a748da69c43ea80aa129550705a5a881d7af926
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-06-04 20:52:24 +0200

    raising width of primary buttons to 100%

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,3 @@ import { application } from "./application";
 
 import DateRangeController from "./date_range_controller";
 application.register("date-range", DateRangeController);
-
-import FlatpickrController from "./flatpickr_controller";
-application.register("flatpickr", FlatpickrController);
