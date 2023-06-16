0fd2835b0ab9180508e6729b8209ffa4728da617
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-06-16 17:02:48 +0200

    reverse default timezone to utc

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Calcometer
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Zurich"
-    config.active_record.default_timezone = :local
+    config.time_zone = "UTC"
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
     config.eager_load_paths << Rails.root.join('app/services')
   end
