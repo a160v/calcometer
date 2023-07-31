cd339e983e068eb6742f40b02bc2ac9d8aa63e42
Aleks Vangjelofski <aleks.vangjelofski@gmail.com>
2023-07-31 19:43:31 +0200

    fixed CSFA token upon login error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery with: :exception
-
   # Make browser_time_zone method available to all views
   helper_method :browser_time_zone
+
 
   # Use the jstz Javascript timezone library to help auto-detect and set the user's time zone
   def browser_time_zone
@@ -22,7 +23,7 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }
   end
 
-  # Set the locale upon login
+  # Set the locale upon log in
   def set_locale
     I18n.locale = current_user&.locale || params[:locale] || I18n.default_locale
   end
@@ -31,7 +32,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :time_zone, :locale, :password) }
-
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :time_zone, :locale, :password) }
   end
 end
