f9e1acebda3b93e12f01e723d3700803b2b25ce1
Aleks Vangjelofski <aleks.vangjelofski@gmail.com>
2023-07-31 12:37:06 +0200

    fixed exposed keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, '401008547865-6f4m5a7fjnaj4l8q76ifaf8loiq5bhp4.apps.googleusercontent.com', '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'
+  config.omniauth :google_oauth2, ENV["GOOGLE_OAUTH_CLIENT_ID"], ENV["GOOGLE_OAUTH_CLIENT_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
