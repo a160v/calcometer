635763febb18af5b470e8c904e3538964744426b
Aleks Vangjelofski <aleks.vangjelofski@gmail.com>
2023-09-29 14:22:48 +0200

    add locale and time_zone default for users signing up with Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   # Associations
@@ -16,14 +16,14 @@ class User < ApplicationRecord
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-        user = User.create(
-          first_name: data['first_name'],
-          last_name: data['last_name'],
-          email: data['email'],
-          password: Devise.friendly_token[0,20]
-        )
-    end
+    user ||= User.create(
+      first_name: data['first_name'],
+      last_name: data['last_name'],
+      time_zone: "Zurich",
+      locale: "it",
+      email: data['email'],
+      password: Devise.friendly_token[0, 20]
+    )
     user
   end
 end
