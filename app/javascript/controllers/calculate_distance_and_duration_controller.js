158526f4ee43e932a64ae4ee147b3d8c2b4cfd3a
Marco Roth <marco.roth@intergga.ch>
2023-10-22 01:26:50 +0200

    Add and use `@rails/request.js`

--- app/javascript/controllers/calculate_distance_and_duration_controller.js
+++ app/javascript/controllers/calculate_distance_and_duration_controller.js
@@ -1,27 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
+import { post } from "@rails/request.js"
 
-// Connects to data-controller="calculate-total-distance-and-duration"
+// Connects to data-controller="calculate-distance-and-duration"
 export default class extends Controller {
   connect() {
     console.log("The 'distance and duration calculator' controller is connected");
   }
 
-  calculate() {
-    let locale = window.location.pathname.split('/')[1];
+  async calculate() {
+    const locale = window.location.pathname.split('/')[1]
+    const url = `/${locale}/appointments/calculate_distance_and_duration`
+    const response = await post(url)
 
-    fetch(`/${locale}/appointments/calculate_distance_and_duration`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").getAttribute('content')
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
+    if (response.ok) {
+      const data = await response.json
+
       // Update the total distance and duration in your view
       document.querySelector("#driving_distance").textContent = `${data.driving_distance} km`;
       document.querySelector("#driving_duration").textContent = `${data.driving_duration} min`;
-      })
-    .catch(error => console.error('Error:', error));
+    } else {
+      console.error("Couldn't update the driving distance and duration")
+    }
   }
 }
